c1ffe3146f4f258e5c698444aa41aa081c110b58
YATUNYI BRIAN ONDEKO <107187750+yatunyi15075@users.noreply.github.com>
2023-12-04 18:51:21 +0300

    Update home.dart

--- codeheal/lib/home.dart
+++ codeheal/lib/home.dart
@@ -1,5 +1,9 @@
-<<<<<<< HEAD
 import 'package:flutter/material.dart';
+import 'doctors_list.dart';
+import 'Ambulance.dart';
+import 'doctors_details.dart';
+import 'profile.dart';
+import 'disease_checker.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -42,16 +46,42 @@ class HomePage extends StatelessWidget {
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                buildBox("Doctor's Appointment", 'assets/img1.jpg'),
-                buildBox("Medicine", 'assets/img1.jpg'),
+                buildTappableBox("Doctor's Appointment", 'assets/img1.jpg', () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => DoctorList()),
+                  );
+                }),
+
+                buildTappableBox("Medicine", 'assets/img1.jpg', () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => DoctorDetails()),
+                  );
+                }),
+
+                // buildBox("Doctor's Appointment", 'assets/img1.jpg'),
+                // buildBox("Medicine", 'assets/img1.jpg'),
               ],
             ),
             SizedBox(height: 20.0),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                buildBox("Health Checker", 'assets/health_checker.png'),
-                buildBox("Ambulance", 'ambulance.png'),
+                buildTappableBox("Health Checker", 'assets/img1.jpg', () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => DiseaseChecker()),
+                  );
+                }),
+                buildTappableBox("Ambulance", 'assets/img1.jpg', () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => AmbulanceSection()),
+                  );
+                }),
+                // buildBox("Health Checker", 'assets/img1.jpg'),
+                // buildBox("Ambulance", 'assets/img1.jpg'),
               ],
             ),
           ],
@@ -81,6 +111,13 @@ class HomePage extends StatelessWidget {
           ),
         ],
       ),
+    );
+  }
+
+  Widget buildTappableBox(String text, String imagePath, VoidCallback onTap) {
+    return InkWell(
+      onTap: onTap,
+      child: buildBox(text, imagePath),
     );
   }
 
@@ -115,121 +152,3 @@ class HomePage extends StatelessWidget {
     );
   }
 }
-=======
-import 'package:flutter/material.dart';
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Row(
-          children: [
-            SizedBox(width: 8.0),
-            Text("Hello Team"),
-            SizedBox(width: 115.0),
-            Icon(Icons.person),
-          ],
-        ),
-      ),
-      body: Padding(
-        padding: EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Search Bar
-            Row(
-              children: [
-                Icon(Icons.search),
-                Expanded(
-                  child: TextFormField(
-                    decoration: InputDecoration(
-                      hintText: "Search",
-                    ),
-                  ),
-                ),
-                Icon(Icons.camera_alt),
-              ],
-            ),
-            SizedBox(height: 80.0),
-
-            //Boxes
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                buildBox("Doctor's Appointment", 'assets/img1.jpg'),
-                buildBox("Medicine", 'assets/img1.jpg'),
-              ],
-            ),
-            SizedBox(height: 20.0),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                buildBox("Health Checker", 'assets/health_checker.png'),
-                buildBox("Ambulance", 'ambulance.png'),
-              ],
-            ),
-          ],
-        ),
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.calendar_today),
-            label: 'Appointment',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.local_pharmacy),
-            label: 'Medicine',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.healing),
-            label: 'Health Checker',
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(Icons.person),
-            label: 'Profile',
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget buildBox(String text, String imgagePath) {
-    return Container(
-      width: 150,
-      height: 150,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        color: Colors.blue,
-      ),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ClipOval(
-            child: Image.asset(
-              imgagePath,
-              width: 75,
-              height: 75,
-              fit: BoxFit.cover,
-            ),
-          ),
-          SizedBox(height: 8),
-          Text(text,
-              textAlign: TextAlign.center,
-              style: TextStyle(
-                color: Colors.white,
-                fontWeight: FontWeight.bold,
-              ))
-        ],
-      ),
-    );
-  }
-}
->>>>>>> 5f693180c6cdf7c0aa1226430fc8aa6a5946ba1f
